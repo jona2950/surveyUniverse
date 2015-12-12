64c738f714080551dba57407c4b47e5c1c8e4c06
Jonathan Fachola <jona2950@gmail.com>
2015-12-12 01:01:01 -0500

    Comment Sections
    Files to work on, currently have comment section only.

---
+++ index.js
@@ -0,0 +1,18 @@
+/** --comment header--
+* @file: index.js (under routing folder)
+* @type: JavaScript
+*
+* homepage routing for main page of application, "Survey Universe"
+* when request is called to domain name for eg, www.surveyuniverse.com
+* 
+* @author Jonathan Fachola-Esnal
+* @name: Survey Universe
+
+* @description: index.js handles request for "Survey Universes" calling it's domain name 
+* "Survey Univereses" homepage is routed to index.ejs under the views folder to be rendered.
+* While able to handle requests @ domain-name "/directive placeholder" such as "About this", "Contact", "Find Survey"
+*/
+
+
+
+
---
+++ survey.js
@@ -0,0 +1,24 @@
+/** --comment header--
+* @file: survey.js (under routing folder)
+* @type: JavaScript
+*
+* homepage routing for main page of application, "Survey Universe"
+* when request is domain address ONLY eg, www.surveyuniverse.com/...
+* 
+* @author Jonathan Fachola-Esnal
+* @name: Survey Universe (routes for edit, create and stats)
+* @description: survey.js handles any requests for creating a new survey, survey editing, 
+* and survey stats viewable to content creator. 
+* @Routes:  "/create", 
+            "/edit?surveyName" (dependent on survey posted to and processed authetication thru route), 
+            "/delete?surveyName" (dependent on survey posted to and processed authetication thru route), 
+            "/stats?surveyName (dependent on survey posted to and processed authetication thru route)" 
+*   
+* survey content edit & deletion - ONLY WHEN - user has ownership of survey, and website administrator
+* are alowed to remove or edit content
+* Chained users to survey are only allowed to edit its contents.
+*/
+
+
+
+

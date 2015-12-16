fa6a11957cb9ae170baa14047046b12497dd2b57
Jonathan Fachola <jona2950@gmail.com>
2015-12-16 12:38:58 -0500

    repeatedly crashing cannot find survey module

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var users = require('./router/survey');
+var users = require('./routes/survey');
 
 var app = express();
 
